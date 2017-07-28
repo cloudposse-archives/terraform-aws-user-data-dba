800aab39d94da512774cda893a8807ce5a4fe475
Igor Rodionov <goruha@gmail.com>
2017-07-28 14:26:31 +0600

    Added bastion support perms

--- main.tf
+++ main.tf
@@ -47,13 +47,7 @@ data "aws_iam_policy_document" "default" {
   }
 
   statement {
-    actions = [
-      "s3:GetObject",
-      "s3:PutObjectAcl",
-      "s3:PutObject",
-      "s3:ListBucket",
-      "s3:DeleteObject",
-    ]
+    actions = [ "s3:*" ]
 
     effect = "Allow"
 
